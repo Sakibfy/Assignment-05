d50da2583723d4a19b8fbc9dee8ef8c650e4786b
Md Sakib <gameingsakib71@gmail.com>
2024-09-23 00:41:55 +0600

    navbar area and main section style done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'btn': '#B4F461',
+      
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+    }
+  },
+}
