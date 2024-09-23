ae80ddf6cb10d045f4cd8472835589caa9c24bbe
Md Sakib <gameingsakib71@gmail.com>
2024-09-24 00:20:18 +0600

    validation done

--- js/app.js
+++ js/app.js
@@ -22,3 +22,9 @@ function reduceAmount(id) {
 function formatCurrency(amout) {
   return `${amout}`;
 }
+
+
+const modal = document.getElementById('my_modal_1')
+function showModal() {
+  
+}
--- js/assignment.js
+++ js/assignment.js
@@ -1,13 +1,19 @@
+// blog btn
+const BlogBtn = document.getElementById('Blog_btn')
+BlogBtn.addEventListener('click', function () {
+  window.location.href = 'blog.html'
+
+})
+
+
+
+// donate btn 1
 document.getElementById('donate_btn_first')
   .addEventListener('click', function () {
     
     const valueInput = inputFieldValue('donate_input_first');
-
-
     const donateTotalAmount = TextFieldValue('donate_total_amount');
-    
-    const newAmount = donateTotalAmount + valueInput;
-    
+    const newAmount = donateTotalAmount + valueInput; 
     document.getElementById('donate_total_amount').innerText = newAmount;
 
     // Reduce Btn
@@ -21,7 +27,7 @@ document.getElementById('donate_btn_first')
 
     const historyItem = document.createElement("div");
     historyItem.className =
-      "bg-white w-10/12 h-36 rounded-md p-8";
+      "bg-white  h-36 rounded-md p-8  mx-auto mt-8 w-10/12 border border-gary-500 rounded-md";
 
     historyItem.innerHTML = `
            
@@ -43,7 +49,6 @@ document.getElementById('donate_btn_sec')
     
     const valueInput = inputFieldValue('donate_input_sec');
 
-
     const donateTotalAmount = TextFieldValue('donate_total_amount_sec');
     
     const newAmount = donateTotalAmount + valueInput;
@@ -61,14 +66,11 @@ document.getElementById('donate_btn_sec')
 
     const historyItem = document.createElement("div");
     historyItem.className =
-      "bg-white w-10/12 h-36 rounded-md p-8";
+      "bg-white  h-36 rounded-md p-8  mx-auto mt-8 w-10/12 border border-gary-500 rounded-md";
 
-    historyItem.innerHTML = `
-           
+    historyItem.innerHTML = `     
     <h2 class="text-2xl text-gray-900 font-bold">${formatCurrency(valueInput)} Taka is Donate for Flood Relief in Feni,Bangladesh</h2>
-    <p class="text-xs text-gray-900 mt-3 inline-block">Date: ${new Date()}</p>
-          
-      `;
+    <p class="text-xs text-gray-900 mt-3 inline-block">Date: ${new Date()}</p> `;
     const historyContainer = document.getElementById("history_area");
 
     historyContainer.insertBefore(historyItem, historyContainer.firstChild);
@@ -80,17 +82,12 @@ document.getElementById('donate_btn_three')
   .addEventListener('click', function () {
     
     const valueInput = inputFieldValue('donate_input_three');
-
-
     const donateTotalAmount = TextFieldValue('donate_total_amount_three');
-    
     const newAmount = donateTotalAmount + valueInput;
-    
     document.getElementById('donate_total_amount_three').innerText = newAmount;
 
     // Reduce Btn
     const totalAmountreduceBtn = reduceAmount('Reduce_Btn');
-    
     const newReduceAmount = totalAmountreduceBtn - valueInput;
     document.getElementById('Reduce_Btn').innerText = newReduceAmount;
 
@@ -99,7 +96,7 @@ document.getElementById('donate_btn_three')
 
     const historyItem = document.createElement("div");
     historyItem.className =
-      "bg-white w-10/12 h-36 rounded-md p-8";
+      "bg-white  h-36 rounded-md p-8  mx-auto mt-8 w-10/12 border border-gary-500 rounded-md";
 
     historyItem.innerHTML = `
            
@@ -110,6 +107,7 @@ document.getElementById('donate_btn_three')
     const historyContainer = document.getElementById("history_area");
 
     historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
   });
 
 // Donation and History
@@ -118,16 +116,17 @@ const donationBtn = document.getElementById('Donation')
 const historyBtn = document.getElementById('History')
 historyBtn.addEventListener('click', function () {
   
-historyBtn.classList.add("bnt_primary");
-donationBtn.classList.remove("bnt_primary");
+  historyBtn.classList.add("bnt_primary");
+  donationBtn.classList.remove("bnt_primary");
   
-historyBtn.classList.remove('border', 'border-gray-400')
-donationBtn.classList.add('border', 'border-gray-400')
+  historyBtn.classList.remove('border', 'border-gray-400')
+  donationBtn.classList.add('border', 'border-gray-400')
 
   
-document.getElementById('main_contant').classList.add('hidden')
+  document.getElementById('main_contant').classList.add('hidden')
 
-})
+  document.getElementById("history_area").classList.remove("hidden");
+});
 
 donationBtn.addEventListener('click', function () {
   historyBtn.classList.remove("bnt_primary");
@@ -138,5 +137,39 @@ donationBtn.addEventListener('click', function () {
 
   
   document.getElementById('main_contant').classList.remove('hidden')
+ document.getElementById("history_area").classList.add("hidden");
+});
 
+
+// live condition check
+document.getElementById("donate_input_first")
+  .addEventListener("input", function () {
+  const inputValue = parseFloat(document.getElementById("donate_input_first").value);
+
+  if (isNaN(inputValue) || inputValue <= 0) {
+    document.getElementById("donate_error_first").classList.remove("hidden");
+    return;
+  }
+  });
+// live condition check 2
+document.getElementById("donate_input_sec")
+  .addEventListener("input", function () {
+  const inputValue = parseFloat(document.getElementById("donate_input_sec").value);
+
+  if (isNaN(inputValue) || inputValue <= 0) {
+    document.getElementById("donate_error_sec").classList.remove("hidden");
+    return;
+  }
+  });
+
+// live condition check 3
+
+document.getElementById("donate_input_three")
+  .addEventListener("input", function () {
+  const inputValue = parseFloat(document.getElementById("donate_input_three").value);
+
+  if (isNaN(inputValue) || inputValue <= 0) {
+    document.getElementById("donate_error_three").classList.remove("hidden");
+    return;
+  }
 });
